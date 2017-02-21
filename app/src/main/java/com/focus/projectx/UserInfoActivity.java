f94e3144d7f29ed6bff8fd96cf08c2e165176791
SFocus <svyatoslav472@gmail.com>
2017-02-21 20:53:15 +0200

    New fragment with user music

--- app/src/main/java/com/focus/projectx/UserInfoActivity.java
+++ app/src/main/java/com/focus/projectx/UserInfoActivity.java
@@ -1,7 +1,15 @@
 package com.focus.projectx;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.focus.projectx.fragments.TopFragment;
+import com.focus.projectx.fragments.TrackFragment;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 public class UserInfoActivity extends AppCompatActivity {
 
@@ -9,5 +17,17 @@ public class UserInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add(R.string.top_track, TopFragment.class)
+                .add(R.string.track, TrackFragment.class)
+                .create());
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter);
+
+        SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
+        viewPagerTab.setViewPager(viewPager);
     }
 }
