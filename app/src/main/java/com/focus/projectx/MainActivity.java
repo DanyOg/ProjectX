f59f3dd5b3452f556a67af61f45005cc4f40112d
DanyOg <meganet0071@gmail.com>
2017-02-24 15:53:40 +0200

    Revert "SignIn && SignUp template"
    This reverts commit 5da04efc52c07ccdae3c626fd0b3a80d2546c1b5.

--- app/src/main/java/com/focus/projectx/MainActivity.java
+++ app/src/main/java/com/focus/projectx/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                Intent intent = new Intent(MainActivity.this, UserInfoActivity.class);
                 startActivity(intent);
             }
         });
