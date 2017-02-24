5da04efc52c07ccdae3c626fd0b3a80d2546c1b5
DanyOg <meganet0071@gmail.com>
2017-02-24 15:52:40 +0200

    SignIn && SignUp template
    SignIn and SingUp Layout add

--- app/src/main/java/com/focus/projectx/LoginActivity.java
+++ app/src/main/java/com/focus/projectx/LoginActivity.java
@@ -1,13 +1,50 @@
 package com.focus.projectx;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.focus.projectx.fragments.SignInFragment;
+import com.focus.projectx.fragments.SignUpDetalFragment;
+import com.focus.projectx.fragments.SignUpFragment;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add(R.string.sign_in, SignInFragment.class)
+                .add(R.string.sign_up, SignUpFragment.class)
+                .create());
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.ChangeTab);
+        viewPager.setAdapter(adapter);
+
+        TabLayout viewPagerTab = (TabLayout) findViewById(R.id.AuthTab);
+        viewPagerTab.setupWithViewPager(viewPager);
+
+    }
+
+    public void DetalAuth(View v) {
+        SignUpDetalFragment detal = new SignUpDetalFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                //.add(R.id., detal)
+                .replace(R.id.ChangeTab, detal)
+                .commit();
+    }
+
+    public void Auth(View v) {
+        Intent intent = new Intent(LoginActivity.this, UserInfoActivity.class);
+        startActivity(intent);
     }
 }
