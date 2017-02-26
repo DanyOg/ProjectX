c70b39a9a5d0d87f48e431865f674ac1b9cef96d
DanyOg <meganet0071@gmail.com>
2017-02-26 20:48:32 +0200

    Add SignUp and SignIn

--- app/src/main/java/com/focus/projectx/MainActivity.java
+++ app/src/main/java/com/focus/projectx/MainActivity.java
@@ -3,17 +3,8 @@ package com.focus.projectx;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
-import com.focus.projectx.HttpLoader.Link;
-import com.focus.projectx.HttpLoader.RetroClient;
-import com.focus.projectx.model.RegisterRequestStatus;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     Button btn;
@@ -26,22 +17,8 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Link link = RetroClient.getApiService();
-                Call<RegisterRequestStatus> call = link.registerNewUser("Svyatoslav", "mail2@mail.com", "123456", "desc");
-                call.enqueue(new Callback<RegisterRequestStatus>() {
-                    @Override
-                    public void onResponse(Call<RegisterRequestStatus> call, Response<RegisterRequestStatus> response) {
-                       RegisterRequestStatus registerRequestStatus = response.body();
-                        Log.d("Log", registerRequestStatus.getStatusMessage());
-                    }
-
-                    @Override
-                    public void onFailure(Call<RegisterRequestStatus> call, Throwable t) {
-
-                    }
-                });
-             //   Intent intent = new Intent(MainActivity.this, UserInfoActivity.class);
-              //  startActivity(intent);
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
             }
         });
     }
